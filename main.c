d8f2f12bb95ee87a2dd8bed44e61f3043f636601
pokgak <m.aimanismail@gmail.com>
2018-08-09 11:33:49 +0200

    initial commit. Copied from examples/gcoap

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "msg.h"
+
+#include "net/gcoap.h"
+#include "kernel_types.h"
+#include "shell.h"
+
+#define MAIN_QUEUE_SIZE (4)
+static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
+
+extern int gcoap_cli_cmd(int argc, char **argv);
+extern void gcoap_cli_init(void);
+
+static const shell_command_t shell_commands[] = {
+    { "coap", "CoAP example", gcoap_cli_cmd },
+    { NULL, NULL, NULL }
+};
+
+int main(void)
+{
+    /* for the thread running the shell */
+    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
+    gcoap_cli_init();
+    puts("gcoap example app");
+
+    /* start shell */
+    puts("All up, running the shell now");
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    /* should never be reached */
+    return 0;
+}
