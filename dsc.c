91574d38cef036179eff2ac8f0a236dc912042a8
pokgak <m.aimanismail@gmail.com>
2018-08-09 11:58:49 +0200

    draft to register sensors to coap

--- dsc.c
+++ dsc.c
@@ -74,3 +74,46 @@ static ssize_t _generic_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, void 
 
     return 0;
 }
+
+/*
+ * Adds dev to the resource array _resources.
+ * Parses the URI from the SAUL_CLASS.
+ * Uses _generic_handler for the handler.
+ */
+static int add_resource(saul_reg_t *dev, int idx)
+{
+    /* Parse the SAUL_CLASS for the URI */
+    char const uri;
+
+    /* Get ops for the device */
+    int ops;
+
+    /* Adds the device to resource list */
+    _resources[idx] = { uri, ops, _generic_handler, NULL };
+
+    return 0;
+}
+
+/*
+ * Finds devices available and the operations it allows.
+ * Returns a struct with the URI for the sensor/resource and allowed operations
+ */
+static void find_devices()
+{
+    int idx = 0;
+
+    /* check if dev already in listener, when not, add to listener */
+    saul_reg_t reg = saul_reg;
+    while (reg->next != NULL) {
+        saul_reg_t dev = reg->next;
+	if (dev not in listener) {
+	    add_resource(&dev, idx);
+	}
+    }
+}
+
+void dsc_init(void)
+{
+    /* TODO: this needs to wait for find_devices() done first */
+    gcoap_register_listener(&_listener);
+}
